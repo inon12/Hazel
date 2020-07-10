8bed0f1160e3bc6bfa76c261b1d3053c3d6c4414
ינון פאר <inon.peer12@gmail.com>
2020-07-10 17:15:51 +0300

    Setup Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//for using by Hazel application
+#include "Hazel/Application.h"
+
+//----Entry Point--------------------
+#include "Hazel/EntryPoint.h"
+//-----------------------------------
